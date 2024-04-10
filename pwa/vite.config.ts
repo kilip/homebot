00b3377792fdb6c7cf9b7ea8795cd5df12d76082
Anthonius Munthi <me@itstoni.com>
2024-04-10 12:19:08 +0800

    initial commit :rocket:

---
+++ pwa/vite.config.ts
@@ -0,0 +1,17 @@
+import { vitePlugin as remix } from "@remix-run/dev";
+import { installGlobals } from "@remix-run/node";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+installGlobals();
+
+export default defineConfig({
+  plugins: [remix(), tsconfigPaths()],
+  server: {
+    strictPort: true,
+    hmr: {
+      clientPort: 5173,
+      protocol: 'ws'
+    }
+  }
+});
